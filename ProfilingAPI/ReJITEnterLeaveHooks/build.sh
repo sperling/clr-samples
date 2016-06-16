1111bcbbf8761cafa4272052f851ce179c2c87e0
Andreas Strid <andreas_strid@bredband.net>
2016-06-16 20:00:58 +0200

    Add clang version environment variable.

--- ProfilingAPI/ReJITEnterLeaveHooks/build.sh
+++ ProfilingAPI/ReJITEnterLeaveHooks/build.sh
@@ -5,17 +5,19 @@
 [ -z "${BuildArch:-}"    ] && BuildArch=x64
 [ -z "${BuildType:-}"    ] && BuildType=Debug
 [ -z "${Output:-}"       ] && Output=CorProfiler.so
+[ -z "${ClangVersion:-}" ] && ClangVersion=3.5
 
 printf '  CORECLR_PATH : %s\n' "$CORECLR_PATH"
 printf '  BuildOS      : %s\n' "$BuildOS"
 printf '  BuildArch    : %s\n' "$BuildArch"
 printf '  BuildType    : %s\n' "$BuildType"
+printf '  ClangVersion : %s\n' "$ClangVersion"
 
 printf '  Building %s ... ' "$Output"
 
 CXX_FLAGS="$CXX_FLAGS --no-undefined -Wno-invalid-noreturn -fPIC -fms-extensions -DBIT64 -DPAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++11"
 INCLUDES="-I $CORECLR_PATH/src/pal/inc/rt -I $CORECLR_PATH/src/pal/prebuilt/inc -I $CORECLR_PATH/src/pal/inc -I $CORECLR_PATH/src/inc -I $CORECLR_PATH/bin/Product/$BuildOS.$BuildArch.$BuildType/inc"
 
-clang++ -shared -o $Output $CXX_FLAGS $INCLUDES ClassFactory.cpp CorProfiler.cpp dllmain.cpp ILRewriter.cpp
+clang++-"$ClangVersion" -shared -o $Output $CXX_FLAGS $INCLUDES ClassFactory.cpp CorProfiler.cpp dllmain.cpp ILRewriter.cpp
 
 printf 'Done.\n'
